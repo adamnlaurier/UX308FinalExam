fa1d01eb14fc96243ef47ac54b18100ce6810791
adamnlaurier <156470632+adamnlaurier@users.noreply.github.com>
2024-04-23 17:02:39 -0400

    Add files via upload

--- src/components.jsx
+++ src/components.jsx
@@ -1,4 +1,121 @@
-import {hello} from './functions.js';
+import {colour_mix, largest_product, day_of_the_week, pay_raise, is_leap} from './functions.js';
+
+function Question1(){
+    return <section>
+         let rgb_colour = "";
+      <h2>results</h2>
+
+      rgb_colour = colour_mix("red", "blue");
+      <p>rgb_colour == "{'fuchsia'}"</p>
+
+      rgb_colour = colour_mix("red", "green");
+      <p>rgb_colour == "{'yellow'}"</p>
+
+      rgb_colour = colour_mix("green", "blue");
+      <p>rgb_colour == "{'aqua'}"</p>
+
+      rgb_colour = colour_mix("red", "red");
+      <p>rgb_colour == "{'red'}"</p>
+
+      rgb_colour = colour_mix("blue", "blue");
+      <p>rgb_colour == "{'blue'}"</p>
+
+      rgb_colour = colour_mix("green", "green");
+      <p>rgb_colour == "{'green'}"</p>
+      
+    </section>;
+}
+
+function Question2(){
+    return <section>
+         let product = 0;
+      <h2>results</h2>
+
+      product = largest_product(-8,12,20);
+      <p>product == "{240}"</p>
+
+      product = largest_product(26,24,25);
+      <p>product == "{650}"</p>
+
+      product = largest_product(442,875,342);
+      <p>product == "{386750}"</p>
+      
+    </section>;
+}
+
+function Question3(){
+    return <section>
+         let day = "";
+      <h2>results</h2>
+
+      day = day_of_the_week(1);
+      <p>day == "{'Sunday'}"</p>
+
+      day = day_of_the_week(4);
+      <p>day == "{'Wednesday'}"</p>
+
+      day = day_of_the_week(6);
+      <p>day == "{'Friday'}"</p>
+      
+    </section>;
+}
+
+function Question4(){
+    return <section>
+         let new_salary = 0;
+      <h2>results</h2>
+      new_salary = pay_raise('F', 12, 120000)
+      <p>new_salary == "{126000}"</p>
+
+      new_salary = pay_raise('F', 3, 80000)
+      <p>new_salary == "{81200}"</p>
+
+      new_salary = pay_raise('P',15,40000);
+      <p>new_salary == "{41200}"</p>
+
+      new_salary = pay_raise('P',2,20000);
+      <p>new_salary == "{20200}"</p>
+
+      new_salary = pay_raise('P', 5, 80000)
+      <p>new_salary == "{81600}"</p>
+      
+    </section>;
+}
+
+function Question5(){
+    return <section>
+         let result = false;
+      <h2>results</h2>
+
+      result = is_leap(1753);
+      <p>result == "{false}"</p>
+
+      result = is_leap(1700);
+      <p>result == "{false}"</p>
+
+      result = is_leap(2004);
+      <p>result == "{true}"</p>
+
+      result = is_leap(1600);
+      <p>result == "{true}"</p>
+      
+    </section>;
+}
+
+export {Question1, Question2, Question3, Question4, Question5}
+/*
+<p>rgb_colour = colour_mix("red", "blue") == ""</p>
+      <p>rgb_colour == "{'fuchsia'}"</p>
+
+
+OLD
+<p>rgb_colour = colour_mix("red", "blue") == "{'fuchsia'}"</p>
+      <p>rgb_colour = colour_mix("red", "green") == "{'yellow'}"</p>
+      <p>rgb_colour = colour_mix("green", "blue") == "{'aqua'}"</p>
+      <p>rgb_colour = colour_mix("red", "red") == "{'red'}"</p>
+      <p>rgb_colour = colour_mix("blue", "blue") == "{'blue'}"</p>
+      <p>rgb_colour = colour_mix("green", "green") == "{'green'}"</p>
+
 
 function Question1(){
     return <section>
@@ -10,4 +127,4 @@ function Question1(){
     </section>;
 }
 
-export {Question1}
+*/
--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,169 @@
-function hello(name){
-    return(`hello ${name}`);
+function colour_mix(rgb_colour1, rgb_colour2){
+    //I decided to use a count system since order doesn't matter.
+    //Easy to discern if theres invalid inputs as the color count should always be 2.
+    let redCount = 0;
+    let greenCount = 0;
+    let blueCount = 0;
+    let finalColour = "Error";
+    
+    if(rgb_colour1 == "red"){
+        redCount++;
+    }else if(rgb_colour1 == "blue"){
+        blueCount++;
+    }else if(rgb_colour1 == "green"){
+        greenCount++;
+    }
+
+    if(rgb_colour2 == "red"){
+        redCount++;
+    }else if(rgb_colour2 == "blue"){
+        blueCount++;
+    }else if(rgb_colour2 == "green"){
+        greenCount++;
+    }
+
+    if(redCount + greenCount + blueCount < 2){
+        finalColour = "Error";
+    }
+
+    if(redCount == 1 && blueCount == 1){
+        finalColour = "fuchsia";
+    }else if(redCount == 1 && greenCount == 1){
+        finalColour = "yellow";
+    }else if(greenCount == 1 && blueCount == 1){
+        finalColour = "aqua";
+    }else if(redCount == 2){
+        finalColour = "red";
+    }else if(blueCount == 2){
+        finalColour = "blue";
+    }else if(greenCount == 2){
+        finalColour = "green";
+    }
+
+    return(finalColour);
 }
 
-export {hello}
+function largest_product(val1, val2, val3){
+    let largeValue1 = 0;
+    let largeValue2 = 0;
+    let product = 0;
+
+    if(val1 > val2){
+        largeValue1 = val1;
+    }else if(val1 < val2){
+        largeValue1 = val2;
+    }else if(val2 < val3){
+        largeValue1 = val3;
+    }
+
+    if(largeValue1 == val1){
+
+        if(val2 > val3){
+            largeValue2 = val2;
+        }else{
+            largeValue2 = val3;
+        }
+        
+    }else if(largeValue1 == val2){
+        
+        if(val1 > val3){
+            largeValue2 = val1;
+        }else{
+            largeValue2 = val3;
+        }
+
+    }else if(largeValue1 == val3){
+        
+        if(val1 > val2){
+            largeValue2 = val1;
+        }else{
+            largeValue2 = val2;
+        }
+
+    }
+
+    product = largeValue1 * largeValue2;
+    return(product);
+}
+
+function day_of_the_week(day_num){
+    let day = "Not valid";
+
+    if(day_num == 1){
+        day = "Sunday";
+    }else if(day_num == 2){
+        day = "Monday";
+    }else if(day_num == 3){
+        day = "Tuesday";
+    }else if(day_num == 4){
+        day = "Wednesday";
+    }else if(day_num == 5){
+        day = "Thursday";
+    }else if(day_num == 6){
+        day = "Friday";
+    }else if(day_num == 7){
+        day = "Saturday";
+    }
+
+    return(day);
+}
+
+function pay_raise(status, years, salary){
+    let new_salary = 0.0;
+
+    if(years >= 10){
+
+        if(status == 'F'){
+            new_salary = salary * 1.05;
+        }else{
+            new_salary = salary * 1.03;
+        }
+
+    }else if(years < 4){
+
+        if(status == 'F'){
+            new_salary = salary * 1.015;
+        }else{
+            new_salary = salary * 1.01;
+        }
+
+    }else{
+        new_salary = salary * 1.02;
+    }
+
+    return(new_salary);
+}
+
+function is_leap(year){
+    /*
+    -------------------------------------------------------
+    Determines if a year is a leap year. Every year that is
+    exactly divisible by four is a leap year, except for years
+    that are exactly divisible by 100, but these centurial years
+    are leap years if they are exactly divisible by 400. For
+    example, the years 1700, 1800, and 1900 are not leap years,
+    but the years 1600 and 2000 are.
+    Use: result = is_leap(year)
+    -------------------------------------------------------
+    Parameters:
+        year - a year (int > 0)
+    Returns:
+        result - True if year is a leap year,
+            False otherwise (boolean)
+    ------------------------------------------------------
+    */
+
+    let result = false;
+
+    if(year % 4 == 0 && year % 100 != 0){
+        result = true;
+    }else if(year % 400 == 0 && year % 100 == 0){
+        result = true;
+    }else{
+        result = false;
+    }
+
+    return(result);
+}
+
+export {colour_mix, largest_product, day_of_the_week, pay_raise, is_leap}
